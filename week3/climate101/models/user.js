994cd89f217ba1c773a377a830bf1858a8cc6b03
Rtheiking1287 <100746144+Rtheiking1287@users.noreply.github.com>
2022-10-23 23:54:02 -0700

    Add files via upload

---
+++ week3/climate101/models/user.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Connect to DB
+mongoose.connect('mongodb://localhost:27017/inventorydb',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+},
+() => console.log("Connected to the DB")
+)
+
+app.get('/', (req, res) => {
+    res.send("TESTING 1, 2, 3...");
+});
+
+
+// Server Listen
+app.listen(3000, () => {
+    console.log("The App is listening on port 3000.")
+});
+
+// Schema
+const inventorySchema = new Schema({
+    item: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 1
+    }
+})
+
+module.exports = mongoose.model('Inventory', inventorySchema)
